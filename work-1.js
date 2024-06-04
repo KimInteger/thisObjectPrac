15c90636c595b92dc5342972dfcb0473fd201acf
KimInteger <kjs20151240@gmail.com>
2024-06-04 10:35:30 +0900

    else의 실행문을 작성함

--- work-1.js
+++ work-1.js
@@ -1,6 +1,11 @@
+// * 논리연산자 &&(and)와 ||(or)
+// * && 둘다 참일 경우에 true, || 둘중 하나만 참이여도 true
+
 function workOne(a,b) {
   if(typeof(a) === 'number' && typeof(b) === 'number'){
     let local = a + b;
     return local;
+  } else {
+    console.error('매개변수 잘못 작성됨');
   }
 };
