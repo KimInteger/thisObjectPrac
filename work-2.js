dc2f5306a1a2551a7d983c2ed07836f665e2eadb
KimInteger <kjs20151240@gmail.com>
2024-06-04 10:47:33 +0900

    생성자함수에 매개변수 a에 setter를 작성

--- work-2.js
+++ work-2.js
@@ -3,6 +3,12 @@ class WorkTwo {
     this.a = a;
     this.b = b;
   }
+
+  set a(value) {
+    if(typeof(value)==='number'){
+      return this._a = value;
+    }
+  }
 }
 
 let test = new WorkTwo('きっさま','야호');
