631b57e52b6f297a34df1894810f95fc7b26867f
KimInteger <kjs20151240@gmail.com>
2024-06-04 11:55:34 +0900

    생성자함수를 리터럴용으로 사용함

--- class.js
+++ class.js
@@ -1,1 +1,11 @@
-console.log('class.js만듦');
+class ItsMe {
+  constructor () {
+    this.name = '인티저';
+    this.age = 29;
+    this.home = '대전';
+  }
+}
+
+let itsMe = new ItsMe();
+
+console.log(itsMe);
