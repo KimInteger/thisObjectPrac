d120df75025eb52aba71c6bcece9208feff429e5
KimInteger <kjs20151240@gmail.com>
2024-06-04 10:50:06 +0900

    error를 생성해서 던질 수 있음. throw new Error를 사용 new Error 객체라는 뜻

--- work-2.js
+++ work-2.js
@@ -7,9 +7,11 @@ class WorkTwo {
   set a(value) {
     if(typeof(value)==='number'){
       return this._a = value;
+    } else {
+      throw new Error('a는 숫자만 입력 가능합니다.');
     }
   }
 }
 
-let test = new WorkTwo('きっさま','야호');
+let test = new WorkTwo(5,'야호');
 console.log(test);
